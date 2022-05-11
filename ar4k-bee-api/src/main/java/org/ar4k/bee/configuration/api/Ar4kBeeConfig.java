00c65bc6441db60d89e707bdaf704628a144b09d
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2022-05-11 21:37:53 +0200

    try build on arm64

---
+++ ar4k-bee-api/src/main/java/org/ar4k/bee/configuration/api/Ar4kBeeConfig.java
@@ -0,0 +1,11 @@
+package org.ar4k.bee.configuration.api;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+public class Ar4kBeeConfig implements Serializable {
+
+	private static final Logger logger = Logger.getLogger(Ar4kBeeConfig.class.getName());
+	private static final long serialVersionUID = -1029985985764843489L;
+
+}
